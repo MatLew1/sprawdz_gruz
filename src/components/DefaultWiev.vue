913f77a4103b02582656541758ec8adae700badf
Lewy <mateuszxlewandowski@gmail.com>
2025-01-26 00:17:59 +0100

    26.01.2025 00:17

---
+++ src/components/DefaultWiev.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <label for="lineNumber">Numer Linii:</label>
+    <line-and-brigade></line-and-brigade>
+  </div>
+</template>
+
+<script>
+
+import LineAndBrigade from "./LineAndBrigade.vue";
+
+export default {
+  components: {
+    LineAndBrigade,
+  },
+};
+</script>
+
+<style scoped>
+label {
+  font-weight: bold;
+}
+</style>
